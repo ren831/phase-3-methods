d56e2675e9bbb8736308047e7af453a9c6b3d698
ren831 <rene83120@gmail.com>
2022-05-05 10:56:27 -0700

    commit tree 6c08d5ad7cca86a69f7f3d1a2f776a30afe50893

--- methods.rb
+++ methods.rb
@@ -1,1 +1,25 @@
 # Your code here!
+def greet_programer
+  puts 'Hello, programmer!'
+  
+end
+
+def greet(name)
+  puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+  return num1 + num2
+end
+
+def halve(x)
+  if x.is_a?(Integer)
+    x/2.0
+  else 
+    nil
+  end
+end
